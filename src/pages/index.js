edff06ea8eaddb5833cb2410f57c1fe68737175c
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-20 11:39:41 -0300

    finishing the input & form style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,34 +4,62 @@ import {
   Flex,
   Box,
   Text,
-  FormControl,
-  FormLabel,
-  Input,
   VStack,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
 } from "@chakra-ui/react";
+import { InputForm } from "../components/input";
 
 export default function Home() {
   return (
     <Box margin="4">
+      {/* Header */}
       <Flex color="white" justifyContent="space-between">
         <Text color="black" fontSize="2xl">
-          Lista de Clientes
+          Crud
         </Text>
         <Button colorScheme="blue">+</Button>
       </Flex>
-      <VStack>
-        <FormControl id="email" marginY="1rem">
-          <FormLabel>Email address</FormLabel>
-          <Input type="email" />
-        </FormControl>
-        <FormControl marginY="1rem">
-          <FormLabel>Name</FormLabel>
-          <Input type="Text" />
-        </FormControl>
-        <Button fontSize="sm" alignSelf="flex-end">
-          Cadastro
+      {/* Input */}
+      <VStack marginBottom="4">
+        <InputForm type="text" label="Name" name="Name" />
+        <InputForm type="email" label="Email" name="Email" />
+
+        <Button colorScheme="blue" fontSize="sm" alignSelf="flex-end">
+          Submit
         </Button>
       </VStack>
+      {/* Table */}
+      <Table variant="simple">
+        <Thead bgColor="blue.500">
+          <Tr>
+            <Th textColor="white">Name</Th>
+            <Th textColor="white">Email</Th>
+            <Th textColor="white">Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Luiz Fernando</Td>
+            <Td>bckainight@nba.com</Td>
+            <Td>
+              <Flex justifyContent="space-between">
+                <Button colorScheme="yellow" size="sm" font-size="smaller">
+                  Edit
+                </Button>
+                <Button colorScheme="red" size="sm" font-size="smaller">
+                  Remove
+                </Button>
+              </Flex>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </Box>
   );
 }
