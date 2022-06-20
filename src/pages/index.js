c4de263cb37d04e07741ea20afd1d4885166a852
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-20 10:55:09 -0300

    input area

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,37 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Box,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box margin="4">
+      <Flex color="white" justifyContent="space-between">
+        <Text color="black" fontSize="2xl">
+          Lista de Clientes
+        </Text>
+        <Button colorScheme="blue">+</Button>
+      </Flex>
+      <VStack>
+        <FormControl id="email" marginY="1rem">
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+        </FormControl>
+        <FormControl marginY="1rem">
+          <FormLabel>Name</FormLabel>
+          <Input type="Text" />
+        </FormControl>
+        <Button fontSize="sm" alignSelf="flex-end">
+          Cadastro
+        </Button>
+      </VStack>
+    </Box>
+  );
 }
