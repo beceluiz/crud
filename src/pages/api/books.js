a3766f68b6cdd83571d297ed5e0e59e4a6f57daa
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-06-27 01:16:14 -0300

    adding create function into api route

--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -13,8 +13,22 @@ export default async function handler(req, res) {
         res.status(200).json(books);
       } catch (err) {
         console.log(err);
+        res.status(500).json({ sucess: false, err });
       }
 
       break;
+
+    case "POST":
+      try {
+        const { title, author } = req.body;
+        if (title && author) throw "invalid data";
+
+        const book = await Book.create({ title, author });
+
+        res.status(201).json(book);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ sucess: false, err });
+      }
   }
 }
