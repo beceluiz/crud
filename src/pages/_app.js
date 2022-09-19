7f278254ed9d58abe17a74225bef4d300eef1d73
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-09-19 11:05:35 -0300

    (feat): add title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider ColorMode="light">
+      <Head>
+        <title>Plan to Read Books</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
