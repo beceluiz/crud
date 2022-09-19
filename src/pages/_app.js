51eb83a6469497762a26fa025ae6657ca4712571
beceluiz <luizfernando.z.l.2011@gmail.com>
2022-09-19 11:17:19 -0300

    (feat): add a favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,25 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider ColorMode="light">
       <Head>
         <title>Plan to Read Books</title>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
